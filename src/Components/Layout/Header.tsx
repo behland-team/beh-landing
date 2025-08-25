3c0cdd6ff2ac1e8bb4443e6af0fef4af713f3735
parham <parhamhassanzade.p@gmail.com>
2025-08-26 00:53:06 +0330

    text color change

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -5,7 +5,7 @@ import logo from "../../../public/images/Logo.png";
 export default function Header() {
   const t = useTranslations("header");
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full  justify-center items-start h-[10dvh]  bg-cream-light">
+    <header className=" flex flex-wrap sm:justify-start sm:flex-nowrap w-full  justify-center items-start h-[10dvh]  bg-cream-light">
       <nav
         dir="rtl"
         className="max-w-[85rem]   mx-auto px-4 sm:flex sm:items-center sm:justify-between bg-header  bg-contain bg-center bg-no-repeat w-4/6 h-full"
@@ -59,7 +59,7 @@ export default function Header() {
         <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
           <span className="icon icon-language text-xl text-cream-medium border-2 rounded-full px-2 p-1 border-cream-medium bg-cream-light shadow-main"></span>
 
-          <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl shadow-main bg-cream-lighthover:bg-cream-medium/80 text-cream-medium font-bold p-2 rounded-2xl border-2 border-cream-medium bg-cream-light flex gap-2 items-center ">
+          <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl shadow-main  hover:bg-cream-medium/80 text-text-orange font-bold p-2 rounded-2xl border-2 border-cream-medium bg-cream-light flex gap-2 items-center ">
             <span className="icon icon-ranking"></span>
             {t("login")}
           </button>
