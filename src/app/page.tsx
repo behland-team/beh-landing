b21a52095cefbebcac002b16d87db4e0b38334e0
parham <parhamhassanzade.p@gmail.com>
2025-08-31 02:16:49 +0330

    character update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import RoadMap from "@/Components/MainPage/RoadMap";
 import Image from "next/image";
 
 export default function Home() {
+
   return (
     <>
       <Banner/>
       <RoadMap />
-      <Characters/>
+      <Characters  />
      
     </>
   );
