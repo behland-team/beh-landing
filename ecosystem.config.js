bf1e4ea6ad149c395baa4dd5e24f13c4a729c2f1
parham <parhamhassanzade.p@gmail.com>
2025-09-08 13:20:41 +0330

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,11 @@
 module.exports = {
     apps: [
         {
-            name: "beh-landing",             // 👈 your app’s name in PM2
-            script: "npm",                  // run with npm
-            args: "start",                  // run "npm start"
-            cwd: "/home/USER/htdocs/behsite.hiteck.ir/beh-landing",                 // current working directory = project root
-            env: {
-                NODE_ENV: "production",
-                PORT: 3000                    // change this if you want another port
-            }
+            name: "beh-landing",
+            script: "npm",
+            args: "start",
+            cwd: "/home/hiteck-behsite/htdocs/behsite.hiteck.ir/beh-landing",
+            env: { NODE_ENV: "production", PORT: 3000 }
         }
     ]
 };
-
-
-
-
-
