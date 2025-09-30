b775a06e02999b34666adf96406152e3f1671484
AbolfazlJM <abolfazljafari563@gmail.com>
2025-09-30 11:47:29 +0330

    feat : make menu link more clean & usable

--- src/Components/Layout/MenuLink.tsx
+++ src/Components/Layout/MenuLink.tsx
@@ -4,16 +4,24 @@ import Link, {LinkProps} from "next/link";
 import classNames from "classnames";
 import {usePathname} from "next/navigation";
 
-interface MenuLinkProps  extends LinkProps{
+interface MenuLinkProps extends LinkProps {
     className?: string;
     children?: React.ReactNode;
+    exact?: boolean;
 }
 
-function MenuLink({href, className, children, ...props}:MenuLinkProps) {
+function MenuLink({
+                      href,
+                      className,
+                      children,
+                      exact = true,
+                      ...props
+                  }: MenuLinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement> & React.RefAttributes<HTMLAnchorElement>) {
     const pathname = usePathname();
+    const isActive = !exact ? pathname.startsWith(href) : pathname === href;
     return (
         <Link {...props}
-              className={classNames("font-medium  lg:text-sm xl:text-base hover:text-cream-medium focus:outline-hidden", className, pathname === href ? "text-cream-medium" : "text-text-black")}
+              className={classNames("font-medium  lg:text-sm xl:text-base hover:text-cream-medium focus:outline-hidden", className, isActive ? "text-cream-medium" : "text-text-black")}
               href={href}>
             {children}
         </Link>
